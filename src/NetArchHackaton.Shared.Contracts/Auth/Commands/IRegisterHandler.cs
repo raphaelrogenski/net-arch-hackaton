31ae06dd12aa76209fba7d473042ef79ec668468
raphaelrogenski <raphaelrogenski@gmail.com>
2025-06-22 19:23:42 -0300

    fix(auth): remove obsolete RegisterHandler and IRegisterHandler to prevent conflicts

--- src/NetArchHackaton.Shared.Contracts/Auth/Commands/IRegisterHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using NetArchHackaton.Shared.Contracts.Auth.DTOs;
-
-namespace NetArchHackaton.Shared.Contracts.Auth.Commands
-{
-    public interface IRegisterHandler
-    {
-        Task<bool> HandleAsync(RegisterRequest request);
-    }
-}
