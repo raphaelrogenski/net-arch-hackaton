85a5091e420a88afaed018ef6cb52298430b1398
raphaelrogenski <raphaelrogenski@gmail.com>
2025-06-28 15:09:45 -0300

    feat(menu): add Product entity, mapping, and database model

--- src/NetArchHackaton.Shared.Domain/Products/Models/Product.cs
+++ src/NetArchHackaton.Shared.Domain/Products/Models/Product.cs
@@ -2,5 +2,10 @@
 {
     public class Product : EntityBase
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public bool IsAvailable { get; set; }
     }
 }
--- src/NetArchHackaton.Shared.Infrastructure/Products/Mappings/ProductMapping.cs
+++ src/NetArchHackaton.Shared.Infrastructure/Products/Mappings/ProductMapping.cs
@@ -8,13 +8,36 @@ namespace NetArchHackaton.Shared.Infrastructure.Products
     {
         public void Configure(EntityTypeBuilder<Product> builder)
         {
-            builder.ToTable("Products", "dbo");
+            builder.ToTable("Products", "menu");
             builder.HasKey(r => r.Id);
 
             builder.Property(r => r.Created)
                 .IsRequired();
 
             builder.Property(r => r.Updated);
+
+            builder.Property(r => r.Name)
+                .IsRequired()
+                .HasColumnType("varchar(255)")
+                .HasMaxLength(255);
+
+            builder.Property(r => r.Description)
+                .IsRequired()
+                .HasColumnType("varchar(255)")
+                .HasMaxLength(255);
+
+            builder.Property(r => r.Price)
+                .IsRequired()
+                .HasColumnType("decimal(10,2)")
+                .HasMaxLength(11);
+
+            builder.Property(r => r.Category)
+                .IsRequired()
+                .HasColumnType("varchar(255)")
+                .HasMaxLength(255);
+
+            builder.Property(r => r.IsAvailable)
+                .IsRequired();
         }
     }
 }
