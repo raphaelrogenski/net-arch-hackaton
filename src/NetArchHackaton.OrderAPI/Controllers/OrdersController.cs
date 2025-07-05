6eb21d00c501aaa6f2071f00784b2b7c18165c21
raphaelrogenski <raphaelrogenski@gmail.com>
2025-07-05 16:11:14 -0300

    feat(api): connect command and query handlers to API controllers

--- src/NetArchHackaton.OrderAPI/Controllers/OrdersController.cs
+++ src/NetArchHackaton.OrderAPI/Controllers/OrdersController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using NetArchHackaton.Shared.Application.Orders.Exceptions;
+using NetArchHackaton.Shared.Contracts.Orders.Commands;
 using NetArchHackaton.Shared.Contracts.Orders.DTOs;
+using NetArchHackaton.Shared.Contracts.Orders.Queries;
+using System.Security.Claims;
 
 namespace NetArchHackaton.MenuAPI.Controllers
 {
@@ -8,36 +12,115 @@ namespace NetArchHackaton.MenuAPI.Controllers
     [Route("[controller]")]
     public class OrdersController : ControllerBase
     {
-        public OrdersController()
+        private readonly IGetOrdersHandler getHandler;
+        private readonly IGetOrderByIdHandler getByIdHandler;
+        private readonly IProduceCreateOrderHandler createHandler;
+        private readonly IProduceCancelOrderHandler cancelHandler;
+
+        public OrdersController(IGetOrdersHandler getHandler, IGetOrderByIdHandler getByIdHandler, IProduceCreateOrderHandler createHandler, IProduceCancelOrderHandler cancelHandler)
         {
+            this.getHandler = getHandler;
+            this.getByIdHandler = getByIdHandler;
+            this.createHandler = createHandler;
+            this.cancelHandler = cancelHandler;
         }
 
         [HttpGet]
         [Authorize(Roles = "Customer")]
         public async Task<IActionResult> Get()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var userEmail = GetUserEmail();
+                var result = await getHandler.HandleAsync(userEmail);
+
+                return Ok(result);
+            }
+            catch (Shared.Application.Base.Exceptions.ApplicationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
         }
 
         [HttpGet("{id}")]
         [Authorize(Roles = "Customer")]
         public async Task<IActionResult> Get(Guid id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var userEmail = GetUserEmail();
+                var result = await getByIdHandler.HandleAsync(userEmail, id);
+
+                return Ok(result);
+            }
+            catch (OrderNotFoundException)
+            {
+                return NotFound();
+            }
+            catch (Shared.Application.Base.Exceptions.ApplicationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
         }
 
         [HttpPost]
         [Authorize(Roles = "Customer")]
         public async Task<IActionResult> Create(CreateOrderRequest request)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var userEmail = GetUserEmail();
+                await createHandler.HandleAsync(userEmail, request);
+
+                return Ok("Request submitted successful!");
+            }
+            catch (Shared.Application.Base.Exceptions.ApplicationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
         }
 
         [HttpPut("{id}/cancel")]
         [Authorize(Roles = "Customer")]
         public async Task<IActionResult> Cancel(Guid id, CancelOrderRequest request)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var userEmail = GetUserEmail();
+                await cancelHandler.HandleAsync(userEmail, id, request);
+
+                return Ok("Request submitted successful!");
+            }
+            catch (OrderNotFoundException)
+            {
+                return NotFound();
+            }
+            catch (Shared.Application.Base.Exceptions.ApplicationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        private string GetUserEmail()
+        {
+            var email = User.FindFirst(ClaimTypes.Email)?.Value;
+            return email;
         }
     }
 }
--- src/NetArchHackaton.OrderAPI/Startup.Application.cs
+++ src/NetArchHackaton.OrderAPI/Startup.Application.cs
@@ -1,9 +1,22 @@
-﻿namespace NetArchHackaton.OrderAPI
+﻿using NetArchHackaton.Shared.Application.Orders.Commands;
+using NetArchHackaton.Shared.Application.Orders.Queries;
+using NetArchHackaton.Shared.Application.Orders.Validators;
+using NetArchHackaton.Shared.Contracts.Orders.Commands;
+using NetArchHackaton.Shared.Contracts.Orders.Queries;
+
+namespace NetArchHackaton.OrderAPI
 {
     public partial class Startup
     {
         private void ConfigureApplication(WebApplicationBuilder builder)
         {
+            builder.Services.AddScoped<IGetOrdersHandler, GetOrdersHandler>();
+            builder.Services.AddScoped<IGetOrderByIdHandler, GetOrderByIdHandler>();
+            builder.Services.AddScoped<IProduceCreateOrderHandler, ProduceCreateOrderHandler>();
+            builder.Services.AddScoped<IProduceCancelOrderHandler, ProduceCancelOrderHandler>();
+
+            builder.Services.AddScoped<OnCreateOrderValidator>();
+            builder.Services.AddScoped<OnCancelOrderValidator>();
         }
     }
 }
