fad2d4bea6f03621176f7b168afaa23047788ff6
raphaelrogenski <raphaelrogenski@gmail.com>
2025-07-06 20:15:24 -0300

    fix(kitchen): add missing KitchenOrderStatusEnum to complete entity definition

---
+++ src/NetArchHackaton.Shared.Domain/KitchenOrders/Models/KitchenOrderStatusEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace NetArchHackaton.Shared.Domain.KitchenOrders
+{
+    public enum KitchenOrderStatusEnum
+    {
+        Pending = 1,
+        Accepted = 2,
+        Rejected = 3,
+        Finished = 4,
+    }
+}
